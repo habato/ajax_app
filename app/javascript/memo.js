0cc572211023c3f457dbd4f4dc93d859d8a68aab
habato <kobatochan22@yahoo.co.jp>
2021-07-09 21:04:18 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,7 +1,7 @@
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
-    e.preventDefault
+    e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
